3b73420b100171b1cbb6ff73dc0b1671b627b90f
이은수 <eunsue@yettiesoft.com>
2018-04-25 13:41:46 +0900

    CHER-10 #comment ErrorHandling 반환값 수정

--- repos/TCP_SEED_BASE64/TCP_SEED_BASE64/tcp_test.c
+++ repos/TCP_SEED_BASE64/TCP_SEED_BASE64/tcp_test.c
@@ -6,26 +6,29 @@
 #include "b64.h"
 #include "seedcbc.h"
 
+#define BUFSIZE 16
+#define BUFSIZE2 10240
+#define ADDRESS "172.16.10.141"
 
 void ErrorHandling(char *message);
 
 void main(){
 
 	// SEED 알고리즘을 이용하여 암/복호화를 수행하기위한 비밀키
-	unsigned char key[16] = { 0xED,0x24,0x01,0xAD, 0x22,0xFA,0x25,0x59,
+	unsigned char key[BUFSIZE] = { 0xED,0x24,0x01,0xAD, 0x22,0xFA,0x25,0x59,
 								0x91,0xBA,0xFD,0xB0, 0x1F,0xEF,0xD6,0x97 };
 
 
 	// CBC운영모드에서 사용되는 초기화벡터
-	unsigned char iv[16] = { 0x93,0xEB,0x14,0x9F, 0x92,0xC9,0x90,0x5B,
+	unsigned char iv[BUFSIZE] = { 0x93,0xEB,0x14,0x9F, 0x92,0xC9,0x90,0x5B,
 								0xAE,0x5C,0xD3,0x4D, 0xA0,0x6C,0x3C,0x8E };
 
 
 	// 암호문을 복호화한 평문을 저장할 변수
-	unsigned char plaintext[10240] = {0x00, };
+	unsigned char plaintext[BUFSIZE2] = {0x00, };
 
 	// 암호문을 저장할 변수
-	unsigned char ciphertext[10256] = {0x00, };
+	unsigned char ciphertext[BUFSIZE2+16] = {0x00, };
 	
 	// Base64 decoding 변수
 	unsigned char *b64_dec = NULL;
@@ -42,38 +45,38 @@ void main(){
 	// base64로 디코딩된 문장의 길이를 저장할 변수
 	size_t b64dec_len = 0;
 
-	WSADATA        wsaData;
-    SOCKET         ServerSocket, ClientSocket;   //소켓을 선언합니다.
-    SOCKADDR_IN    ServerAddress, ClientAddress;  //소켓의 주소 정보를 넣는 구조체입니다.
-    unsigned short ServerPort = 5005;
-
 	/*
 	
 	* nRcv : Recieve한 메시지 길이 변수
 	* AddressSize : ClientAddress size 변수
 
 	*/
-	int nRcv, AddressSize;
+	int nRcv = 0, AddressSize = 0;
 
 	//char exit_value[7] = {0x00, };
 
+	WSADATA        wsaData;
+    SOCKET         ServerSocket, ClientSocket;   //소켓을 선언합니다.
+    SOCKADDR_IN    ServerAddress, ClientAddress;  //소켓의 주소 정보를 넣는 구조체입니다.
+    unsigned short ServerPort = 5005;
+
     if (WSAStartup(MAKEWORD(2,2),&wsaData) == SOCKET_ERROR)
-        ErrorHandling( "WSAStartup Error.....\n" );
+        return ErrorHandling( "WSAStartup Error.....\n" );
 
 	ServerAddress.sin_family = AF_INET;
-    ServerAddress.sin_addr.s_addr = inet_addr( "172.16.10.141" );
+    ServerAddress.sin_addr.s_addr = inet_addr( ADDRESS );
 	ServerAddress.sin_port = htons( ServerPort );  //포트번호
 
 	ServerSocket = socket(AF_INET, SOCK_STREAM,0);
 
 	if( ServerSocket == INVALID_SOCKET ) //에러 발생시 문구 출력.
-		ErrorHandling( "Socket Creation Error....." );
+		return ErrorHandling( "Socket Creation Error....." );
 
 	if( bind(ServerSocket,(struct sockaddr*)&ServerAddress,sizeof(ServerAddress) ) == SOCKET_ERROR ) 
-        ErrorHandling( "Bind Error......" );
+        return ErrorHandling( "Bind Error......" );
 
 	if( listen(ServerSocket,SOMAXCONN) == SOCKET_ERROR ) 
-		ErrorHandling( "Listen Error.....\n" );
+		return ErrorHandling( "Listen Error.....\n" );
 
 
 	// socket accept
@@ -83,7 +86,7 @@ void main(){
 
 
 	if( (ClientSocket = accept( ServerSocket,(struct sockaddr*)&ClientAddress , &AddressSize )) == INVALID_SOCKET )
-		ErrorHandling( "Accept Error.....\n" );
+		return ErrorHandling( "Accept Error.....\n" );
 	else
 	{
 		printf("Connect IP: %s, Port : %d\n", inet_ntoa(ClientAddress.sin_addr), htons(ClientAddress.sin_port)) ;
@@ -92,11 +95,12 @@ void main(){
 
 	closesocket( ServerSocket ); //소켓을 닫습니다.
 
+	// socket receive/send
 	while(1){
 		printf("Message Receives ...\n");
 
-		memset(plaintext, '\0', 10240);
-		memset(ciphertext, '\0', 10256);
+		memset(plaintext, '\0', BUFSIZE2);
+		memset(ciphertext, '\0', BUFSIZE2+16);
 		//memset(exit_value, '\0', 7);
 
 		nRcv = recv(ClientSocket, (char*)ciphertext, sizeof(ciphertext) -1, 0);
@@ -149,6 +153,7 @@ void ErrorHandling(char *message){
 	WSACleanup();
 	fputs(message, stderr);
 	fputc('\n',stderr);
-	getchar();
+	//getchar();
 	exit(1);
+	return;
 }
